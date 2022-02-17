ddc752715795a30a2a9611de69e943d5c58807d0
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-02-17 20:13:52 -0300

    getting to know static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { stripe } from '../services/stripe';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+
+// 3 types of API calls:
+// Client-side - faster/less resourceful, not indexed
+// Server-side - makes screen load more expensive, is indexed
+// Static Site Generation - less resourceful but dont update data on the go
+
+// Example: Blog post
+// Content - Can be SSG, will be the same for everyone
+// Comments - Can be Client-side, this shouldnt make the screen take more time to load
 
 interface HomeProps {
   product: {
@@ -39,7 +48,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KUIEYDgfDeh2bSaOjo0G9Xi');
 
   const product = {
@@ -53,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
