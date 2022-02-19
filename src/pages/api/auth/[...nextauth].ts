9f39d4e05a8aecc960a5f965781ec07c4fc997d6
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-02-19 19:10:48 -0300

    saving user on faunadb with next auth callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,10 @@
+import { query as q } from 'faunadb';
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,6 +15,23 @@ export default NextAuth({
         },
       },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+
+        return true
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 })
