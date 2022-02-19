608edf2a0b9869d28cb5cac49c8be5f7a0ba9262
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-02-19 10:48:15 -0300

    authentication strategies

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (request: NextApiRequest, response: NextApiResponse) => {
+// Authenication strategies:
+// JWT (saves data on storage)
+// Next Auth (uses social authentication apis ie, google, github, discord)
+// Cognito, Auth0 (authentication services for more advanced authentication)
+
+const Users = (request: NextApiRequest, response: NextApiResponse) => {
  const users = [
    { id: 1, name: 'John'},
    { id: 2, name: 'Emily'},
@@ -9,3 +14,5 @@ export default (request: NextApiRequest, response: NextApiResponse) => {
 
  return response.json(users);
 }
+
+export default Users;
