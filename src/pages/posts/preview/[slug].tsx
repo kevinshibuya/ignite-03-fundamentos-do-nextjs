78954b473f072528b1d4ddb81b627915310eee89
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-02-22 16:34:46 -0300

    creating post preview page

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,85 @@
+import { GetStaticProps } from "next"
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import * as PrismicHelper from '@prismicio/helpers';
+import { useEffect } from "react";
+
+import Client from "../../../utils/prismicHelpers";
+
+import styles from "../post.module.scss";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const {data: session} = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session]);
+  
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={`${styles.postContent} ${styles.previewContent}`} dangerouslySetInnerHTML={{ __html: post.content }} />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = Client();
+
+  const response = await prismic.getByUID<any>('post', String(slug), {});
+
+  const post = {
+    slug,
+    title: PrismicHelper.asText(response.data.title),
+    content: PrismicHelper.asHTML(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('en-US', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    }),
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
